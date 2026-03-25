261223c52257582905e402b906ead57628c57a70
huynd4104 <huy412004@gmail.com>
2026-03-25 11:35:30 +0700

    reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart' hide ChangeNotifierProvider;
 import 'package:provider/provider.dart';
